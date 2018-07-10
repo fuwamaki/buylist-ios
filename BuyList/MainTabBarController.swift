04fef7c299589adc4b205e49ae62314c49edfad3
fuwamaki <variety.fuwa.3@gmail.com>
2018-07-11 00:38:37 +0900

    MainTabBarControllerを追加&storyboard結びつけ

---
+++ BuyList/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  BuyList
+//
+//  Created by 牧宥作 on 2018/07/11.
+//  Copyright © 2018 牧 宥作. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 0
+    }
+}
