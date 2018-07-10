17d7199ee1f4927e7834af61969bd11aa89a6519
fuwamaki <variety.fuwa.3@gmail.com>
2018-07-11 01:15:19 +0900

    MainTabBarControllerにnavigationItemのタイトル表示ロジックを追加

--- BuyList/MainTabBarController.swift
+++ BuyList/MainTabBarController.swift
@@ -10,8 +10,41 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
 
+    enum TabNumber: Int {
+        case buylist = 0
+        case history = 1
+        case setting = 2
+    }
+
+    struct TabItem {
+        let title: String
+        let tabNumber: TabNumber
+
+        init(title: String, tabNumber: TabNumber) {
+            self.title = title
+            self.tabNumber = tabNumber
+        }
+    }
+
+    var tabItems: [TabItem] {
+        return [TabItem(title: "買い物", tabNumber: .buylist),
+                TabItem(title: "履歴", tabNumber: .history),
+                TabItem(title: "設定", tabNumber: .setting)]
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.selectedIndex = 0
+        setTabView()
+    }
+
+    private func setTabView() {
+        selectedIndex = tabItems.startIndex
+        navigationItem.title = tabItems[tabItems.startIndex].title
+    }
+
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if let index = tabBar.items?.firstIndex(of: item) {
+            navigationItem.title = tabItems[index].title
+        }
     }
 }
