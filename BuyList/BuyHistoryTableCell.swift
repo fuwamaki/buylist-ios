9a22ec41bf4ffb2a8059bc7965960b5bbdaf763e
fuwamaki <yusaku_maki@fablic.co.jp>
2018-07-11 19:34:15 +0900

    BuyListTableCellを追加

---
+++ BuyList/BuyHistoryTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  BuyHistoryTableCell.swift
+//  BuyList
+//
+//  Created by y-maki on 2018/07/11.
+//  Copyright © 2018年 牧 宥作. All rights reserved.
+//
+
+import UIKit
+
+final class BuyHistoryTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
