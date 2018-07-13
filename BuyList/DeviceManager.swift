aac0e1975f758da45c9ec58b529f938b2d9d6338
fuwamaki <yusaku_maki@fablic.co.jp>
2018-07-13 19:31:02 +0900

    DeviceManagerクラスを追加&advertisingIdentifierを取得するfunc作ってみた

---
+++ BuyList/DeviceManager.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceManager.swift
+//  BuyList
+//
+//  Created by y-maki on 2018/07/13.
+//  Copyright © 2018年 牧 宥作. All rights reserved.
+//
+
+import Foundation
+import AdSupport
+
+class DeviceManager {
+    
+    func getAdvertisingIdentifier() -> String {
+        return ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    }
+}
