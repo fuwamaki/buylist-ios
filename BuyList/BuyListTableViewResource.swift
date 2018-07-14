e364a0d5aee4e473c1a97e5be7d87e47e736e029
fuwamaki <yusaku_maki@fablic.co.jp>
2018-07-15 00:53:41 +0900

    BuyListTableViewResourceの書き方を大幅に修正

--- BuyList/BuyListTableViewResource.swift
+++ BuyList/BuyListTableViewResource.swift
@@ -9,21 +9,55 @@
 import Foundation
 
 struct BuyListTableViewResource {
-    var buyListContentSectionInfo = BuyListContentSectionInfo()
-    var buyListAddContentSectionInfo = BuyListAddContentSectionInfo()
 
-    private var sectionList: [BuyListSectionInfo] {
-        return [buyListContentSectionInfo, buyListAddContentSectionInfo]
+    private struct Constant {
+        static let addTitle = "項目を追加"
+    }
+    struct TableSection {
+        let title: String?
+        let cells: [TableCell]
+
+        init(title: String? = nil, cells: [TableCell]) {
+            self.title = title
+            self.cells = cells
+        }
     }
 
-    var sectionListCount: Int {
-        return sectionList.count
+    struct TableCell {
+        let title: String?
+        let type: BuyListTableCellType
+
+        init(title: String? = nil, type: BuyListTableCellType) {
+            self.title = title
+            self.type = type
+        }
     }
 
-    subscript (index: Int) -> BuyListSectionInfo? {
-        guard sectionList.count > index else {
+    var contentCells: [TableCell] = []
+    let addCell = TableCell(title: Constant.addTitle, type: .add)
+
+    private var tableSections: [TableSection] {
+        let contentSection = TableSection(cells: contentCells)
+        let addSection = TableSection(cells: [addCell])
+        return [contentSection, addSection]
+    }
+
+    var tableSectionCount: Int {
+        return tableSections.count
+    }
+
+    subscript (index: Int) -> TableSection? {
+        guard tableSections.count > index else {
             return nil
         }
-        return sectionList[index]
+        return tableSections[index]
+    }
+
+    mutating func appendContent(_ item: ItemEntity) {
+        contentCells.append(TableCell(title: item.itemName, type: .content))
+    }
+
+    mutating func removeContent(_ index: Int) {
+        contentCells.remove(at: index)
     }
 }
