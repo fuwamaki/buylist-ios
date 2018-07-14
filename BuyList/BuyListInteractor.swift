8eadd47f482ea6673ac651442bf2976c07425eb3
fuwamaki <yusaku_maki@fablic.co.jp>
2018-07-15 03:04:31 +0900

    InteractorでRealmのgetとsaveができるように

--- BuyList/BuyListInteractor.swift
+++ BuyList/BuyListInteractor.swift
@@ -9,18 +9,35 @@
 import Foundation
 
 protocol BuyListInteractable {
-    func getBuyListData() -> [ItemEntity]
+    func getItems() -> [ItemEntity]
+    func saveItem(name: String, count: Int)
+    func getBuyListMockData() -> [ItemEntity]
 }
 
 class BuyListInteractor: BuyListInteractable {
 
-    let loadJsonFile = LoadJsonFile()
+    private let itemRealm = ItemRealm()
 
-    func getBuyListData() -> [ItemEntity] {
+    private struct Constant {
+        static let none = "なし"
+    }
+
+    func getItems() -> [ItemEntity] {
+        return itemRealm.realmAction(action: .find) ?? []
+    }
+
+    func saveItem(name: String, count: Int) {
+        let itemRealmEntity = ItemRealmEntity(value: ["buyId": "0002", "itemId": "0002", "name": name, "count": count, "createTime": Date()])
+        let _ = itemRealm.realmAction(action: .save, item: itemRealmEntity)
+    }
+
+    // mock
+    let loadJsonFile = LoadJsonFile()
+    func getBuyListMockData() -> [ItemEntity] {
         var items: [ItemEntity] = []
         if let jsonDate = loadJsonFile.getJson() {
             jsonDate.rows.forEach {
-                items.append(ItemEntity(buyId: "1", itemId: $0.itemId, itemName: $0.name, count: $0.count, createTime: Date()))
+                items.append(ItemEntity(buyId: "1", itemId: $0.itemId, name: $0.name, count: $0.count, createTime: Date()))
             }
         }
         return items
