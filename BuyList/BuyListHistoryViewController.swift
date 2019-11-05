1d67ad4059ad7de91a0370e0e3bcbe8dddae0eab
fuwamaki <yusaku.maki@street-academy.com>
2019-11-05 10:11:28 +0900

    fix crash about register BuyListHistoryCell

--- BuyList/BuyListHistoryViewController.swift
+++ BuyList/BuyListHistoryViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class BuyListHistoryViewController: UIViewController {
 
     private var eventHandler: BuyListHistoryEventHandler?
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet private weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
